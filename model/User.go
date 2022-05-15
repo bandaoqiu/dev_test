fb8df42753849560d12daf3f94252e6ce1e79b32
bandaoqiu <xixishu84@126.com>
2022-05-15 15:27:50 +0800

    test firstName 2 to 64

--- model/User.go
+++ model/User.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	FirstName string `json:"first_name" binding:"required,min=2" gorm:"column:first_name;type:varchar(255);not null;"`
-	LastName  string `json:"last_name" binding:"required,min=2" gorm:"column:last_name;type:varchar(255);not null"`
+	FirstName string `json:"first_name" binding:"required,min=64" gorm:"column:first_name;type:varchar(255);not null;"`
+	LastName  string `json:"last_name" binding:"required,min=64" gorm:"column:last_name;type:varchar(255);not null"`
 	Email     string `json:"email" binding:"required,email" gorm:"column:email;type:varchar(200);not null;uniqueIndex"`
 	Password  string `json:"password" binding:"required,min=6,max=16,alphanum" gorm:"column:password;type:varchar(100);not null;"`
 }
